11e7779c3101339b4e34b6bb89db4b0ea940dd89
SKeoughCode <131558651+SKeoughCode@users.noreply.github.com>
2023-04-23 20:49:53 -0400

    Final Objects Commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+
+const employee = {
+    name: `Joe Blank`,
+    streetAddress: `123 Sesame Street`,
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const temp = { ...employee };
+    temp[key] = value;
+    return temp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    const temp = { ...employee };
+    delete temp[key];
+    return temp;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
